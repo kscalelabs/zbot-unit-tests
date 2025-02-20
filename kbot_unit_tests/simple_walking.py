00c3ba2ac3288917ce984f1a4b33d66735bd73eb
Ali K <ali@kscale.dev>
2025-02-20 08:38:20 +0000

    stilll works

--- kbot_unit_tests/simple_walking.py
+++ kbot_unit_tests/simple_walking.py
@@ -8,6 +8,8 @@ import subprocess
 import time
 from dataclasses import dataclass
 
+import yaml
+import numpy as np
 import colorlogging
 import numpy as np
 import onnx
@@ -19,6 +21,39 @@ from pykos import KOS
 from scipy.spatial.transform import Rotation as R
 
 logger = logging.getLogger(__name__)
+
+async def load_policy_and_config(checkpoint_path: str) -> tuple[ort.InferenceSession, dict]:
+    try:
+        # List YAML and ONNX files in the given directory
+        yaml_files = [f for f in os.listdir(checkpoint_path) if f.endswith("env.yaml")]
+        policy_files = [f for f in os.listdir(checkpoint_path) if f.endswith(".onnx")]
+
+        if not yaml_files or not policy_files:
+            raise FileNotFoundError(f"Could not find env.yaml and .onnx files in {checkpoint_path}")
+
+        yaml_file = yaml_files[0]  # Use the first found YAML file
+        policy_file = policy_files[0]  # Use the first found ONNX file
+
+        policy_path = os.path.join(checkpoint_path, policy_file)
+        yaml_path = os.path.join(checkpoint_path, yaml_file)
+
+        logger.info("Loading policy from: %s", os.path.abspath(policy_path))
+        logger.info("Loading config from: %s", os.path.abspath(yaml_path))
+
+        # Load the policy and create an inference session
+        policy = onnx.load(policy_path)
+        session = ort.InferenceSession(policy.SerializeToString())
+
+        with open(yaml_path, "r") as f:
+            config = yaml.load(f, Loader=yaml.Loader)
+
+        logger.info("Loaded policy and config")
+
+        return session, config
+
+    except Exception as e:
+        logger.error("Error in load_policy_and_config: %s", e)
+        raise
 
 
 def get_gravity_orientation(quaternion: np.ndarray) -> np.ndarray:
@@ -34,6 +69,30 @@ def get_gravity_orientation(quaternion: np.ndarray) -> np.ndarray:
     gravity_orientation[2] = 1 - 2 * (qw * qw + qz * qz)
 
     return gravity_orientation
+
+
+start_pos = {
+    "left_shoulder_pitch_03": 0.0,
+    "left_shoulder_roll_03": 0.0,
+    "left_shoulder_yaw_02": 0.0,
+    "left_elbow_02": -math.radians(90),
+    "left_wrist_02": 0.0,
+    "right_shoulder_pitch_03": 0.0,
+    "right_shoulder_roll_03": 0.0,
+    "right_shoulder_yaw_02": 0.0,
+    "right_elbow_02": math.radians(90),
+    "right_wrist_02": 0.0,
+    "left_hip_pitch_04": math.radians(20),  # Slight forward lean
+    "left_hip_roll_03": 0.0,  # Neutral stance
+    "left_hip_yaw_03": 0.0,
+    "left_knee_04": math.radians(40),  # Bent knees for stability
+    "left_ankle_02": -math.radians(20),  # Compensate for knee bend
+    "right_hip_pitch_04": -math.radians(20),
+    "right_hip_roll_03": 0.0,
+    "right_hip_yaw_03": 0.0,
+    "right_knee_04": -math.radians(40),
+    "right_ankle_02": math.radians(20),
+}
 
 
 @dataclass
@@ -96,6 +155,56 @@ ACTUATOR_ID_TO_POLICY_IDX = {
     45: 19,  # right_ankle_02
 }
 
+isaac_joint_names = [
+    "left_hip_pitch_04",
+    "left_shoulder_pitch_03",
+    "right_hip_pitch_04",
+    "right_shoulder_pitch_03",
+    "left_hip_roll_03",
+    "left_shoulder_roll_03",
+    "right_hip_roll_03",
+    "right_shoulder_roll_03",
+    "left_hip_yaw_03",
+    "left_shoulder_yaw_02",
+    "right_hip_yaw_03",
+    "right_shoulder_yaw_02",
+    "left_knee_04",
+    "left_elbow_02",
+    "right_knee_04",
+    "right_elbow_02",
+    "left_ankle_02",
+    "left_wrist_02",
+    "right_ankle_02",
+    "right_wrist_02",
+]
+
+class RobotState:
+    """Tracks robot state and handles offsets."""
+
+    def __init__(
+        self,
+        joint_names: list[str],
+        start_pos: dict[str, float],
+        vel_cmd: np.ndarray,
+        config: dict,
+        policy: ort.InferenceSession,
+        idtoname: dict[int, str],
+        nametoid: dict[str, int],
+        indextoname: dict[int, str],
+        nametoindex: dict[str, int],
+    ):
+        self.joint_offsets = {name: 0.0 for name in joint_names}
+        self.orn_offset = None
+        self.start_pos = start_pos
+        self.vel_cmd = vel_cmd
+        self.config = config
+        self.policy = policy
+        self.idtoname = idtoname
+        self.nametoid = nametoid
+        self.indextoname = indextoname
+        self.nametoindex = nametoindex
+        self.prev_action = np.zeros(len(joint_names), dtype=np.float32)
+
 
 def map_isaac_to_mujoco(isaac_to_mujoco_mapping, isaac_position) -> np.ndarray:
     """Maps joint positions from Isaac format to MuJoCo format.
@@ -129,13 +238,28 @@ def map_mujoco_to_isaac(mujoco_to_isaac_mapping, mujoco_position):
     return isaac_position
 
 
-async def simple_walking(
-    policy: Any,
-    default_position: list[float], 
-    host: str, 
-    port: int
+async def run_policy(
+    args: argparse.Namespace
 ) -> None:
-    async with KOS(ip=host, port=port) as sim_kos:
+    
+    policy, config = await load_policy_and_config(args.model_path)
+    default_position = [
+        0.0, 0.0, 0.0, -1.5707963267948966, 0.0, 0.0, 0.0, 0.0, 1.5707963267948966, 0.0, 
+        0.3490658503988659, 0.0, 0.0, 0.6981317007977318, -0.3490658503988659, -0.3490658503988659, 0.0, 0.0,
+        -0.6981317007977318, 0.3490658503988659
+    ]
+
+        # Create mappings between actuator ID and joint name
+    idtoname = {actuator.actuator_id: actuator.joint_name for actuator in ACTUATOR_LIST}
+    nametoid = {actuator.joint_name: actuator.actuator_id for actuator in ACTUATOR_LIST}
+
+    # Example policy joint mapping (assumes policy outputs match the order of joints)
+    # Assuming your policy output corresponds to the joints in a specific order (as per ACTUATOR_LIST)
+    indextoname = {i: isaac_joint_names[i] for i in range(len(isaac_joint_names))}
+    nametoindex = {isaac_joint_names[i]: i for i in range(len(isaac_joint_names))}
+
+
+    async with KOS(ip=args.host, port=args.port) as sim_kos:
         # USE CONFIGURE ACTUATOR AT YOUR OWN RISK 
 
         for actuator in ACTUATOR_LIST: 
@@ -145,8 +269,8 @@ async def simple_walking(
                 kd=actuator.kd,
                 max_torque=actuator.max_torque
             )
-        # Initialize position and orientation
-        base_pos = [0.0, 0.0, 1.05]  # x, y, z
+        # # Initialize position and orientation
+        base_pos = [0.0, 0.0, 1.15]  # x, y, z
         base_quat = [1.0, 0.0, 0.0, 0.0]  # w, x, y, z
         
         # Create joint values list in the format expected by sim_pb2.JointValue
@@ -159,6 +283,18 @@ async def simple_walking(
             pos={"x": base_pos[0], "y": base_pos[1], "z": base_pos[2]},
             quat={"w": base_quat[0], "x": base_quat[1], "y": base_quat[2], "z": base_quat[3]},
             joints=joint_values
+        )
+
+        robot_state = RobotState(
+            joint_names=[actuator.joint_name for actuator in ACTUATOR_LIST],
+            start_pos=start_pos,
+            vel_cmd=np.zeros(20),
+            config=config,
+            policy=policy,
+            idtoname=idtoname,
+            nametoid=nametoid,
+            indextoname=indextoname,
+            nametoindex=nametoindex
         )
 
 
@@ -178,7 +314,7 @@ async def simple_walking(
 
         x_vel_cmd = 0.0
         y_vel_cmd = 0.0
-        yaw_vel_cmd = -0.1
+        yaw_vel_cmd = 0.0
         frequency = 50
 
         mujoco_to_isaac_mapping = {
@@ -304,31 +440,14 @@ async def main() -> None:
  
     colorlogging.configure(level=logging.DEBUG if args.debug else logging.INFO)
 
-    # Get the most recent yaml and onnx files from the checkpoint directory
-    yaml_files = [f for f in os.listdir(args.model_path) if f.endswith('env.yaml')]
-    policy_files = [f for f in os.listdir(args.model_path) if f.endswith('.onnx')]
     
-    if not yaml_files or not policy_files:
-        raise FileNotFoundError(f"Could not find env.yaml and .onnx files in {args.model_path}")
-
-    yaml_file = yaml_files[0]  # Use first found yaml
-    policy_file = policy_files[0]  # Use first found onnx
-    policy_path = os.path.join(args.model_path, policy_file)
-    yaml_path = os.path.join(args.model_path, yaml_file)
 
     try:
         print("Running in simulation-only mode...")
 
-        policy = onnx.load(policy_path)
-        session = ort.InferenceSession(policy.SerializeToString())
 
-        default_position = [
-            0.0, 0.0, 0.0, -1.5707963267948966, 0.0, 0.0, 0.0, 0.0, 1.5707963267948966, 0.0, 
-            0.3490658503988659, 0.0, 0.0, 0.6981317007977318, -0.3490658503988659, -0.3490658503988659, 0.0, 0.0,
-            -0.6981317007977318, 0.3490658503988659
-        ]
 
-        await simple_walking(session, default_position, args.host, args.port)
+        await run_policy(args)
 
     except Exception:
         logger.exception("Simulator error")
